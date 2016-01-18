2e2427be011e7d92f53dd0f231aa35297f9f9b45
kanekotic <alvarojosepl@gmail.com>
2016-01-18 08:51:19 +0100

    add routing on login and logout

--- client/routes.js
+++ client/routes.js
@@ -1,7 +1,24 @@
+Accounts.onLogin(function(){
+  FlowRouter.go('video-list')
+});
+
+Accounts.onLogout(function(){
+  FlowRouter.go('home')
+});
+
+FlowRouter.triggers.enter([function(context, redirect){
+  if(!Meteor.userId()){
+    FlowRouter.go('home')
+  }
+}]);
+
 FlowRouter.route('/',
 {
   name: 'home',
   action(){
+    if(Meteor.userId()){
+      FlowRouter.go('video-list')
+    }
     GAnalytics.pageview();
     BlazeLayout.render('home-view');
   }
@@ -18,7 +35,7 @@ FlowRouter.route('/video-list',
 
 FlowRouter.route('/playlist/:id',
 {
-  name: 'video-list',
+  name: 'video',
   action(){
     GAnalytics.pageview();
     BlazeLayout.render('main-view', {main: 'PlaylistDetails'});
